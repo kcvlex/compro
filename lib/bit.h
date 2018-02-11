9ea7d8d9c3d72af4707ac5ad2f36fab1ad800172
kcvlex <krkr9893@gmail.com>
2018-02-12 07:05:25 +0900

    change a little, behavior does not change

--- lib/bit.h
+++ lib/bit.h
@@ -4,33 +4,29 @@ class Bit{
         long long *data;
 
     public:
-        Bit(long long);
-        long long sum(long long);
-        void add(long long, long long);
+        Bit(long long n){
+			long long newsize = 1;
+			while(newsize < n)  newsize = newsize << 1;
+			size = newsize + 1;
+			data = new long long[size];
+			for(long long i = 0; i < size; ++i){
+				data[i] = 0;
+			}
+		}
+        
+		long long sum(long long pos){
+			long long ret = 0;
+			while(pos > 0){
+				ret += data[pos];
+				pos -= pos & -pos;
+			}
+			return ret;
+		}
+
+		void add(long long pos, long long delta){
+			while(pos <= size){
+				data[pos] += delta;
+				pos += pos & -pos;
+			}
+		}
 };
-
-Bit::Bit(long long n){
-    long long newsize = 1;
-    while(newsize < n)  newsize = newsize << 1;
-    size = newsize + 1;
-    data = new long long[size];
-    for(long long i = 0; i < size; ++i){
-        data[i] = 0;
-    }
-}
-
-long long Bit::sum(long long pos){
-    long long ret = 0;
-    while(pos > 0){
-        ret += data[pos];
-        pos -= pos & -pos;
-    }
-    return ret;
-}
-
-void Bit::add(long long pos, long long delta){
-    while(pos <= size){
-        data[pos] += delta;
-        pos += pos & -pos;
-    }
-}
