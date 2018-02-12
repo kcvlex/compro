bbdb6e913dcd77f80ea458245a4b3499cde2365c
kcvlex <krkr9893@gmail.com>
2018-02-13 04:53:45 +0900

    change a little, not change behavior

--- lib/unionfind.h
+++ lib/unionfind.h
@@ -1,49 +1,46 @@
-class UnionFind{
-public:
-    long long size;
-    long long *par;
-    long long *rank;
+#include<cstdint>
+namespace{
+	class UnionFind{
+		public:
+			int64_t size;
+			int64_t *par;
+			int64_t *rank;
 
-    UnionFind(long long);
-    ~UnionFind();
-    long long find(long long);
-    void unit(long long, long long);
-    bool same(long long, long long);
-};
+			UnionFind(int64_t n){
+				size = n;
+				par = new int64_t[n];
+				rank = new int64_t[n];
+				for(int i = 0; i < n; ++i){
+					par[i] = i;
+					rank[i] = 0;
+				}
+			}
 
-UnionFind::UnionFind(long long n){
-    size = n;
-    par = new long long[n];
-    rank = new long long[n];
-    for(int i = 0; i < n; ++i){
-        par[i] = i;
-        rank[i] = 0;
-    }
-}
+			~UnionFind(){
+				delete[] par;
+				delete[] rank;
+			}
 
-UnionFind::~UnionFind(){
-    delete[] par;
-    delete[] rank;
-}
+			int64_t find(int64_t child){
+				return (child == par[child] ? child : par[child] = find(par[child]));
+			}
 
-long long UnionFind::find(long long child){
-    return (child == par[child] ? child : par[child] = find(par[child]));
-}
+			void unit(int64_t x, int64_t y){
+				x = find(x); y = find(y);
+				if(x != y){
+					if(rank[x] > rank[y]){
+						par[y] = x;
+					}else{
+						par[x] = y;
+						if(rank[x] == rank[y]){
+							++rank[y];
+						}
+					}
+				}
+			}
 
-void UnionFind::unit(long long x, long long y){
-    x = find(x); y = find(y);
-    if(x != y){
-        if(rank[x] > rank[y]){
-            par[y] = x;
-        }else{
-            par[x] = y;
-            if(rank[x] == rank[y]){
-                ++rank[y];
-            }
-        }
-    }
-}
-
-bool UnionFind::same(long long x, long long y){
-    return (find(x) == find(y));
+			bool same(int64_t x, int64_t y){
+				return (find(x) == find(y));
+			}
+	};
 }
