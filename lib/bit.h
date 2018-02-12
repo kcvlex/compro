4ba4441ba604654bce38d342a97f8b77700ed060
kcvlex <krkr9893@gmail.com>
2018-02-13 03:44:03 +0900

    0-origin

--- lib/bit.h
+++ lib/bit.h
@@ -1,32 +1,38 @@
-class Bit{
-    private:
-        long long size;
-        long long *data;
+#include<cstdint>
+namespace{
+	class Bit{
+		private:
+			int64_t size;
+			int64_t *data;
 
-    public:
-        Bit(long long n){
-			long long newsize = 1;
-			while(newsize < n)  newsize = newsize << 1;
-			size = newsize + 1;
-			data = new long long[size];
-			for(long long i = 0; i < size; ++i){
-				data[i] = 0;
+		public:
+			Bit(int64_t n){
+				int64_t newsize = 1;
+				while(newsize < n)  newsize = newsize << 1;
+				size = newsize + 1;
+				data = new int64_t[size];
+				for(int64_t i = 0; i < size; ++i){
+					data[i] = 0;
+				}
 			}
-		}
-        
-		long long sum(long long pos){
-			long long ret = 0;
-			while(pos > 0){
-				ret += data[pos];
-				pos -= pos & -pos;
-			}
-			return ret;
-		}
 
-		void add(long long pos, long long delta){
-			while(pos <= size){
-				data[pos] += delta;
-				pos += pos & -pos;
+			int64_t sum(long long pos){
+				pos++;
+				int64_t ret = 0;
+				while(pos > 0){
+					ret += data[pos];
+					pos -= pos & -pos;
+				}
+				return ret;
 			}
-		}
-};
+
+			void add(int64_t pos, long long newValue){
+				int64_t delta = newValue - data[pos];
+				pos++;
+				while(pos <= size){
+					data[pos] += delta;
+					pos += pos & -pos;
+				}
+			}
+	};
+}
