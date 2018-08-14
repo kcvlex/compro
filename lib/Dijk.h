647958ced6870e86809c17c1f550b2fbfc96e801
kcvlex <krkr9893@gmail.com>
2018-08-15 07:42:21 +0900

    change : the type of distance, int64_t -> template

--- lib/Dijk.h
+++ lib/Dijk.h
@@ -1,31 +1,34 @@
 #include<bits/stdc++.h>
+using namespace std;
+using ll = int64_t;
 class Dijk{
-    using namespace std;
-    using ll = int64_t;
     private:
         ll n;
-        const vector<pair<ll, ll>> v;
+        vector<vector<pair<ll, T>>> v;
     public:
-        const ll INF = INT64_MAX;
+        T INF;
         
-        Dijk(ll n, const vector<pair<ll, ll>> &v){
-            this->n = n;
+        Dijk(const vector<vector<pair<ll, T>>> &v, T INF){
+            this->n = v.size();
             this->v = v;
+            this->INF = INF;
         }
         
         vector<ll> dijk(ll start){
-            vector<ll> d(n);
-            for(ll i = 0; i < n; i++) d[i] = INF * (i != start);
-            priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, functional<bool(pair<ll, ll>, pair<ll, ll>)>> q([&](pair<ll, ll> a, pair<ll, ll> b){return a.second < b.second;});
-            q.push(pair<ll, ll>(start, 0));
+            vector<ll> d(n, INF);
+            d[start] = 0;
+            priority_queue<pair<ll, T>, vector<pair<ll, T>>, function<bool(pair<ll, T>, pair<ll, T>)>> q([&](pair<ll, T> a, pair<ll, T> b){return a.second > b.second;});
+            q.push(pair<ll, T>(start, 0));
             while(q.size()){
-                pair<ll, ll> now = q.top();
+                pair<ll, T> now = q.top();
                 q.pop();
-                d[now.first] = now.second;
-                for(auto next : v){
+                if(d[now.first] < now.second){
+                    continue;
+                }
+                for(auto next : v[now.first]){
                     if(d[next.first] == INF || d[now.first] + next.second < d[next.first]){
                         d[next.first] = d[now.first] + next.second;
-                        q.push(pair<ll, ll>(next.first, d[now.first] + next.second));
+                        q.push(pair<ll, T>(next.first, d[now.first] + next.second));
                     }
                 }
             }
