ac9bd19fc1612763910fdab245cab2bc46433930
kcvlex <krkr9893@gmail.com>
2019-02-15 02:46:32 +0900

    fix bit

--- lib/bit.cpp
+++ lib/bit.cpp
@@ -1,35 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = int64_t;
+template <typename T> using V = vector<T>;
+template <typename T> using VV = vector<V<T>>;
+
 template <typename T>
 class Bit{
     private:
-        ll size;
-        T *data;
-        T *arr;
+        V<T> data;
         T identity_ele;
 
     public:
 
-        Bit(ll size, T identity_ele){
+        Bit(ll size, T identity_ele) {
             ll newsize = 1;
             while(newsize < size)  newsize = newsize << 1;
-            this->size = newsize + 1;
             this->identity_ele = identity_ele;
-            data = new ll[this->size];
-            arr = new ll[size];
-            for(ll i = 0; i < size; ++i){
-                data[i] = identity_ele;
-                arr[i] = identity_ele;
-            }
+            data = V<T>(newsize + 1, identity_ele);
         }
 
         /*
          * 0-origin
          */
-        T sum(ll pos){
+        T sum(ll pos) {
+            pos++;
             T ret = identity_ele;
-            while(pos > 0){
+            while(pos > 0) {
                 ret += data[pos];
                 pos -= pos & -pos;
             }
@@ -39,17 +35,12 @@ class Bit{
         /*
          * 0-origin
          */
-        void add(ll pos, T delta){
-            arr[pos] += delta;
+        void add(ll pos, T delta) {
             pos++;
-            while(pos <= size){
+            while(pos <= data.size()) {
                 data[pos] = data[pos] + delta;
                 pos += pos & -pos;
             }
         }
-
-        void update(ll pos, T value){
-            T delta = value - arr[pos];
-            this->add(pos, delta);
-        }
 };
+
