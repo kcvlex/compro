2c42d8538df62edb8ddbdd0dd556e81027cd2f62
kcvlex <krkr9893@gmail.com>
2019-05-15 16:22:53 +0900

    generate random input

---
+++ gen.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = int64_t;
+
+
+struct GenNum {
+    random_device rd;
+    mt19937 gen;
+    uniform_int_distribution<ll> dis;
+
+    GenNum(ll l, ll r)
+        : rd(),
+          gen(rd()),
+          dis(l, r - 1) {}
+
+    ll gen_num() {
+        return dis(gen);
+    }
+};
+
+int main() {
+    GenNum gn(0, 7 + 1);
+    for(ll i = 0; i < 30; i++) cout << gn.gen_num() << endl;
+    return 0;
+}
