1c1885d59fe30362f3d67a64d481f2fbc9f39fb3
kcvlex <krkr9893@gmail.com>
2019-07-11 22:49:53 +0900

    impl : splay tree

--- tree/splaytree.cpp
+++ tree/splaytree.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 using ll = int64_t;
-template <typename T> using V = vector<T>;
-template <typename T> using VV = V<V<T>>;
+using ull = uint64_t;
 
 template <typename Key>
 struct SplayTree {
@@ -11,8 +9,9 @@ struct SplayTree {
     struct Node__ {
         using node_ptr__ = shared_ptr<Node__<T>>;
         node_ptr__ parent, left, right;
+        size_t subtree_size;
         T key;
-        Node__(T key, node_ptr__ parent, node_ptr__ left, node_ptr__ right) : key(key), parent(parent), left(left), right(right) {}
+        Node__(T key, node_ptr__ parent, node_ptr__ left, node_ptr__ right) : key(key), parent(parent), left(left), right(right), subtree_size(1) {}
         Node__(T key) : Node__(key, nullptr, nullptr, nullptr) {}
         Node__() : Node__(T()) {}
     };
@@ -29,127 +28,184 @@ struct SplayTree {
     {
     }
 
-    bool is_root(node_ptr node) { return node->parent == nullptr; }
+    bool is_root(node_ptr node) { return node == nullptr || node->parent == nullptr; }
     bool is_left(node_ptr node) { return node->parent->left == node; }
-    void update_root(node_ptr node) { if(node->parent == nullptr) root = node; }
+    size_t get_subtree_size(node_ptr node) { return node == nullptr ? 0 : node->subtree_size; }
+    void update_subtree_size(node_ptr node) { if(node) node->subtree_size = get_subtree_size(node->left) + get_subtree_size(node->right) + 1; }
+    void set_root(node_ptr node) { root = node; if(root) root->parent = nullptr; }
+    void update_root(node_ptr node) { if(!node->parent) root = node; }
 
-    node_ptr make_node(Key key, node_ptr parent, node_ptr left, node_ptr right) {
-        node_ptr ret = make_shared<Node>(key, parent, left, right);
-        return ret;
-    }
-
-    // is_root(idx) must not be true
-    void rot_right(node_ptr cur_node) {
+    void rotate(node_ptr cur_node, bool left) {
         auto par_node = cur_node->parent;
-        cur_node->parent = par_node->parent;
-        par_node->parent = cur_node;
-        par_node->left = cur_node->right;
-        cur_node->right = par_node;
+        set_by_key(par_node->parent, cur_node);
+        if(left) {
+            set_right(par_node, cur_node->left);
+            set_left(cur_node, par_node);
+        } else {
+            set_left(par_node, cur_node->right);
+            set_right(cur_node, par_node);
+        }
+        update_subtree_size(par_node);
+        update_subtree_size(cur_node);
         update_root(cur_node);
     }
 
-    void rot_left(node_ptr cur_node) {
-        auto par_node = cur_node->parent;
-        cur_node->parent = par_node->parent;
-        par_node->parent = cur_node;
-        par_node->right = cur_node->left;
-        cur_node->left = par_node;
-        update_root(cur_node);
+    void set_by_key(node_ptr par, node_ptr ch) {
+        if(ch) ch->parent = par;
+        if(par && ch) {
+            if(comp(par->key, ch->key)) set_right(par, ch);
+            else set_left(par, ch);
+        }
+    }
+
+    void set_left(node_ptr par, node_ptr ch) {
+        if(par) par->left = ch;
+        if(ch) ch->parent = par;
+    }
+
+    void set_right(node_ptr par, node_ptr ch) {
+        if(par) par->right = ch;
+        if(ch) ch->parent = par;
+    }
+
+    void set_children(node_ptr par, node_ptr left, node_ptr right) {
+        set_left(par, left);
+        set_right(par, right);
     }
 
     // is_root(get_parent(idx)) must be true
-    // rot_right is execed if par.left == idx
+    // rotate_right is execed if is_left(cur_node) is true.
     void zig(node_ptr cur_node, bool left) {
-        if(left) rot_right(cur_node);
-        else rot_left(cur_node);
+        rotate(cur_node, !left);
     }
 
     void zig_zig(node_ptr cur_node, bool left) {
-        auto func = (left ? rot_right : rot_left);
-        func(cur_node->parent);
-        func(cur_node);
+        rotate(cur_node->parent, !left);
+        rotate(cur_node, !left);
     }
 
     void zig_zag(node_ptr cur_node, bool left) {
-        auto fst_func = (left ? rot_right : rot_left);
-        auto snd_func = (left ? rot_left : rot_right);
-        fst_func(cur_node);
-        snd_func(cur_node);
+        rotate(cur_node, !left);
+        rotate(cur_node, left);
     }
 
-    void splay(node_ptr cur_node) {
-        while(cur_node != root) {
-            if(root->left == cur_node) {
-                zig(cur_node, left);
-                break;
-            } else if(root->right == cur_node) {
-                zig(cur_node, right);
+    void splay(node_ptr node) {
+        while(!is_root(node)) {
+            if(is_root(node->parent)) {
+                zig(node, is_left(node));
                 break;
             }
-            auto par_node = cur_node->parent;
-            bool cur_left = is_left(cur_node);
-            bool par_left = is_left(par_node);
-            if(cur_left ^ par_left) zig_zag(cur_node, cur_left);
-            else zig_zig(cur_node, cur_left);
+            bool cur_state = is_left(node);
+            bool par_state = is_left(node->parent);
+            if(cur_state ^ par_state) zig_zag(node, cur_state);
+            else zig_zig(node, cur_state);
         }
     }
 
-    node_ptr find(Key key) {
-        auto cur_node = root;
+    node_ptr search(Key key) {
+        node_ptr cur_node = root;
         while(true) {
-            if(cur_node.key == key) break;
-            if(comp(key, cur_node->key) && cur_node->left != nullptr) {
-                auto nxt_node = cur_node->left;
-                if(nxt_node.key != key && comp(nxt_node.key, key)) break;
-                else cur_node = nxt_node;
-            } else if(!comp(key, cur_node->key) && cur_node->right != nullptr) {
-                cur_node = cur_node->right;
+            if(cur_node->key == key) break;
+            if(comp(key, cur_node->key)) {
+                if(cur_node->left) cur_node = cur_node->left;
+                else break;
             } else {
-                break;
+                if(cur_node->right) cur_node = cur_node->right;
+                else break;
             }
         }
-        splay(cur_node);
         return cur_node;
     }
 
-    node_ptr find_max(node_ptr cur_node) {
-        auto ret = cur_node;
-        while(ret->right) ret = ret->right;
-        return ret;
+    node_ptr find(Key key) {
+        auto ret = search(key);
+        splay(ret);
+        return ret->key == key ? ret : nullptr;
     }
 
     void insert(Key key) {
         if(root == nullptr) {
-            root = make_node(key, nullptr, nullptr, nullptr);
+            root = make_shared<Node>(key, nullptr, nullptr, nullptr);
             return;
         }
-        if(find(key)) return;
-        auto node = make_node(key, nullptr, nullptr, nullptr);
-        root->parent = node;
-        if(comp(key, root->key)) {
-            node->left = root->left;
-            root->left = nullptr;
-            node->right = root;
-        } else {
-            node->right = root->right;
-            root->right = nullptr;
-            node->left = root;
+        auto node = search(key);
+        if(node->key != key) {
+            auto nnode = make_shared<Node>(key, nullptr, nullptr, nullptr);
+            set_by_key(node, nnode);
+            node = nnode;
         }
+        splay(node);
     }
 
     void erase(Key key) {
-        auto lower = find(key);
-        if(!find(key)) return;
+        auto node = search(key);
+        if(node->key != key) return;
+        splay(node);
         if(!root->left) {
-            root = root->right;
-            root->parent = nullptr;
+            set_root(root->right);
+        } else if(!root->left->right) {
+            set_right(root->left, root->right);
+            set_root(root->left);
         } else {
-            auto left_max = find_max(root->left);
-            left_max->parent->left = nullptr;
-            left_max->left = root->left;
-            left_max->right = root->right;
-            left_max->parent = nullptr;
-            root = left_max;
+            node_ptr nxt_root = root->left;
+            while(nxt_root->right) {
+                nxt_root->subtree_size--;
+                nxt_root = nxt_root->right;
+            }
+            set_right(nxt_root->parent, nxt_root->left);
+            
+            set_children(nxt_root, root->left, root->right);
+            set_root(nxt_root);
         }
+        update_subtree_size(root);
+    }
+
+    // this method is defined for https://atcoder.jp/contests/arc033/tasks/arc033_3
+    Key query(ll X) {
+        auto cur_node = root;
+        while(X) {
+            size_t left_size = get_subtree_size(cur_node->left);
+            if(left_size == X - 1) break;
+            if(left_size + 1 < X) {
+                X -= left_size + 1;
+                cur_node = cur_node->right;
+            } else {
+                cur_node = cur_node->left;
+            }
+        }
+        auto ret = cur_node->key;
+        splay(cur_node);
+        erase(ret);
+        return ret;
+    }
+
+    void dump(node_ptr node, string label = "") {
+        cout << label << " : " << node->key << ", " << get_subtree_size(node) << endl;
+        if(node->left) dump(node->left, label + 'l');
+        if(node->right) dump(node->right, label + 'r');
+    }
+
+    void dump() {
+        cout << "==== begin dump ====\n";
+        dump(root);
+        cout << "==== end dump ====\n";
     }
 };
+
+// solution for https://atcoder.jp/contests/arc033/tasks/arc033_3
+
+#define VERIFY
+#ifdef VERIFY
+int main() {
+    SplayTree<ll> sp_tree([](ll a, ll b) { return a < b; });
+    ll Q;
+    cin >> Q;
+    while(Q--) {
+        ll T, X;
+        cin >> T >> X;
+        if(T == 1) sp_tree.insert(X);
+        else cout << sp_tree.query(X) << endl;
+    }
+    return 0;
+}
+#endif
