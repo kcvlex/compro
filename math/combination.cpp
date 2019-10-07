85af8526dc8769c7fd885cfd4e3a299d38badef6
kcvlex <krkr9893@gmail.com>
2019-10-07 18:33:08 +0900

    fix : english

--- math/combination.cpp
+++ math/combination.cpp
@@ -6,24 +6,24 @@ template <ll MOD>
 class Combination {
 private:
     ll N;
-    V<ll> factv, rfactv;
+    V<ll> factv, ifactv;
 
 public:
-    Combination<MOD>(ll N) : N(N), factv(N + 1, 1), rfactv(N + 1) {
+    Combination<MOD>(ll N) : N(N), factv(N + 1, 1), ifactv(N + 1) {
         for(ll i = 1; i <= N; i++) factv[i] = factv[i - 1] * i % MOD;
-        rfactv.back() = inv(factv.back());
-        for(ll i = N - 1; 0 <= i; i--) rfactv[i] = (i + 1) * rfactv[i + 1] % MOD;
+        ifactv.back() = inv(factv.back());
+        for(ll i = N - 1; 0 <= i; i--) ifactv[i] = (i + 1) * ifactv[i + 1] % MOD;
     }
 
     ll fact(ll n) { return factv[n]; }
 
-    ll rfact(ll n) { return rfactv[n]; }
+    ll ifact(ll n) { return ifactv[n]; }
 
     ll pow(ll a, ll b) { return b ? (b & 1 ? a : 1) * pow(a * a % MOD, b / 2) % MOD : 1; }
 
     ll inv(ll a) { return pow(a, MOD - 2); }
 
-    ll comb(ll n, ll k) { return factv[n] * rfactv[n - k] % MOD * rfactv[k] % MOD; }
+    ll comb(ll n, ll k) { return factv[n] * ifactv[n - k] % MOD * ifactv[k] % MOD; }
 };
 
 
