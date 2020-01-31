df00dc8d6c4e4840640e51cc34a18e28000c11e1
kcvlex <krkr9893@gmail.com>
2020-02-01 08:46:19 +0900

    lowlink

--- graph/lowlink.cpp
+++ graph/lowlink.cpp
@@ -12,27 +12,26 @@ struct LowLink {
         : N(edges.size()), edges(edges), low(edges.size()), ord(edges.size()), 
           visited(edges.size()), is_articulation(edges.size()), dfs_counter(0)
     {
-        for(ll i = 0; i < N; i++) if(!visited[i]) dfs(i, -1);
+        for (ll i = 0; i < N; i++) if (!visited[i]) dfs(i, -1);
     }
 
-    ll dfs(ll cur, ll pre) {
+    void dfs(ll cur, ll pre) {
         visited[cur] = true;
         ord[cur] = dfs_counter++;
         low[cur] = ord[cur];
         ll children = 0;
         bool art = false;
-        for(ll nxt : edges[cur]) {
-            if(!visited[nxt]) {
+        for (ll nxt : edges[cur]) {
+            if (!visited[nxt]) {
                 children++;
-                ll res = dfs(nxt, cur);
+                dfs(nxt, cur);
                 chmin(low[cur], low[nxt]);
                 art |= (pre == -1 ? 2 <= children : ord[cur] <= low[nxt]);
-            } else {
+            } else if (nxt != pre) {
                 chmin(low[cur], ord[nxt]);
             }
         }
         is_articulation[cur] = art;
-        return ord[cur];
     }
 
     bool is_art(ll cur) { return is_articulation[cur]; }
@@ -42,12 +41,35 @@ struct LowLink {
 
     V<PLL> enum_bridges() {
         V<PLL> ret;
-        for(ll from = 0; from < N; from++) {
-            for(ll to : edges[from]) {
-                if(!(from < to)) continue;
-                if(is_bridge(from, to)) ret.emplace_back(from, to);
+        for (ll from = 0; from < N; from++) {
+            for (ll to : edges[from]) {
+                if (!(from < to)) continue;
+                if (is_bridge(from, to)) ret.emplace_back(from, to);
             }
         }
         return ret;
     }
 };
+
+int main() {
+    ll N, M;
+    cin >> N >> M;
+    VV<ll> edges(N);
+    for (ll i = 0; i < M; i++) {
+        ll a, b;
+        cin >> a >> b;
+        edges[a].push_back(b);
+        edges[b].push_back(a);
+    }
+
+    for (auto &&v : edges) sort(ALL(v));
+
+    LowLink lowlink(edges);
+
+    // for (ll i = 0; i < N; i++) if (lowlink.is_art(i)) cout << i << endl;
+    for (ll i = 0; i < N; i++) for (ll j : edges[i]) {
+        if (!(i < j)) continue;
+        if (lowlink.is_bridge(i, j) || lowlink.is_bridge(j, i)) cout << i << ' ' << j << endl;
+    }
+    return 0;
+}
