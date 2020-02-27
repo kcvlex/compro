ad27cc3d3777e4fff1b3b12a72f2478b2cac74e3
kcvlex <krkr9893@gmail.com>
2020-02-28 01:14:47 +0900

    scc

--- graph/scc.cpp
+++ graph/scc.cpp
@@ -7,8 +7,7 @@ template <typename Graph>
 class StronglyConnectedComponents {
     const Graph &graph;
     Graph rgraph;
-    vec<ll> label;
-    vec<ll> result;
+    vec<ll> label, scc_ord;
 
     void dfs1(ll cur, ll &l) {
         label[cur] = -2;
@@ -26,38 +25,53 @@ class StronglyConnectedComponents {
         for (ll i = 0; i < graph.size(); i++) if (label[i] == -1) dfs1(i, l);
     }
 
-    void dfs2(ll cur, ll l) {
+    void dfs2(ll cur, ll l, ll &idx, vec<ll> &result) {
         result[cur] = l;
+        scc_ord[idx++] = cur;
         for (const graph::Edge &e : rgraph[cur]) {
             ll nxt;
             std::tie(nxt, std::ignore) = e;
             if (result[nxt] != -1) continue;
-            dfs2(nxt, l);
+            dfs2(nxt, l, idx, result);
         }
     }
 
-    void build_scc() {
+    vec<ll> build_scc() {
         ll l = 0;
-        vec<ll> ord(rgraph.size());
+        vec<ll> result(graph.size(), -1), ord(rgraph.size());
         std::iota(ALL(ord), 0ll);
         std::sort(ALL(ord), [&](ll i, ll j) { return label[i] > label[j]; });
-        for (ll n : ord) if (result[n] == -1) dfs2(n, l++);
+        ll idx = 0;
+        for (ll n : ord) if (result[n] == -1) dfs2(n, l++, idx, result);
+        return result;
     }
 
 public:
     StronglyConnectedComponents(const Graph &graph)
-        : graph(graph), label(graph.size(), -1), result(graph.size(), -1) 
+        : graph(graph), label(graph.size(), -1), scc_ord(graph.size())
     {
         rgraph = graph.build_inv();
     }
 
     vec<ll> build() {
+        vec<ll> result(graph.size(), -1);
         write_label();
-        build_scc();
-        return result;
+        return build_scc();
+    }
+
+    std::pair<::graph::Graph<true>, vec<ll>> build_scc_graph() {
+        auto scc_label = build();
+        ::graph::Graph<true> scc_graph(*max_element(ALL(scc_label)) + 1);
+        for (ll from = 0; from < graph.size(); from++) for (auto &&e : graph[from]) {
+            ll to;
+            std::tie(to, std::ignore) = e;
+            ll lf = scc_label[from], lt = scc_label[to];
+            if (lf != lt) scc_graph.add_edge(lf, lt);
+        }
+        return std::make_pair(std::move(scc_graph), std::move(scc_label));
     }
 };
 
-}
+template <typename Graph> using scc = StronglyConnectedComponents<Graph>;
 
-using SCC = graph::StronglyConnectedComponents<graph::Graph<true>>;
+}
--- test/graph/scc.cpp
+++ test/graph/scc.cpp
@@ -1,25 +1,54 @@
 #include "../../graph/scc.cpp"
 
-// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_C&lang=jp
+// https://atcoder.jp/contests/arc030/tasks/arc030_3
+using namespace std;
 int main() {
-    ll N, M;
-    cin >> N >> M;
-    graph::Graph<true> g(N);
-    while (M--) {
-        ll s, t;
-        cin >> s >> t;
-        g.add_edge(s, t);
+    ll n, m, k;
+    cin >> n >> m >> k;
+    vec<char> alp(n);
+    graph::Graph<true> graph(n);
+    for (auto &&e : alp) cin >> e;
+    for (ll i = 0; i < m; i++) {
+        ll a, b;
+        cin >> a >> b;
+        graph.add_edge(a - 1, b - 1);
     }
 
-    SCC scc(g);
-    auto group = scc.build();
-    ll Q;
-    cin >> Q;
-    while (Q--) {
-        ll u, v;
-        cin >> u >> v;
-        bool ans = (group[u] == group[v]);
-        cout << (ans ? 1 : 0) << endl;
+    graph::scc<graph::Graph<true>> builder(graph);
+    graph::Graph<true> cgraph;
+    vec<ll> label;
+    tie(cgraph, label) = builder.build_scc_graph();
+    vvec<ll> nodes(cgraph.size());
+    for (ll i = 0; i < n; i++) nodes[label[i]].push_back(i);
+    const ssize_t SIZE = 310;
+    string dp[SIZE][SIZE];
+    for (ll i = 0; i < nodes.size(); i++) {
+        priority_queue<char, vec<char>, greater<char>> pq;
+        for (ll n : nodes[i]) pq.push(alp[n]);
+        ll ok = 0;
+        for (; ok < SIZE && dp[i][ok].size() == ok; ok++);
+        for (ll maxv = ok; maxv < SIZE && pq.size(); maxv++) {
+            char c = pq.top();
+            pq.pop();
+            for (ll j = maxv; 1 <= j; j--) {
+                if (dp[i][j].size() == 0) dp[i][j] = dp[i][j - 1] + c;
+                else chmin(dp[i][j], dp[i][j - 1] + c);
+            }
+        }
+        for (auto &&e : cgraph[i]) {
+            ll succ;
+            tie(succ, ignore) = e;
+            for (ll j = 1; j < SIZE && dp[i][j].size(); j++) {
+                auto &v = dp[succ][j];
+                if (v.size() == 0) dp[succ][j] = dp[i][j];
+                else chmin(dp[succ][j], dp[i][j]);
+            }
+        }
     }
+
+    const string inf(SIZE, 'z');
+    string ans = inf;
+    for (ll i = 0; i < SIZE; i++) if (dp[i][k].size() == k) chmin(ans, dp[i][k]);
+    cout << (ans == inf ? "-1" : ans) << endl;
     return 0;
 }
