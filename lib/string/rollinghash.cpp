d8cf211875da1ce843bfa24d4ff943ca785d3e43
kcvlex <krkr9893@gmail.com>
2020-03-20 20:51:02 +0900

    roliha

--- lib/string/rollinghash.cpp
+++ lib/string/rollinghash.cpp
@@ -3,152 +3,137 @@
 
 namespace str {
 
-namespace {
-
-template <typename T, std::size_t Size, typename ...Args>
-struct sized_tuple {
-    using type = typename sized_tuple<T, Size - 1, T, Args...>::type;
-};
-
-template <typename T, typename ...Args>
-struct sized_tuple<T, 0, Args...> {
-    using type = std::tuple<Args...>;
-};
-
-template <std::size_t Size, std::size_t Count, typename BidirIterator, typename ...Args>
-constexpr auto arr2tuple(BidirIterator ite, Args&&... args) {
-    if constexpr (Size == Count) {
-        return std::make_tuple(args...);
-    } else {
-        auto cpy = *ite;
-        return arr2tuple<Size, Count + 1>(std::next(ite), cpy, std::forward<Args>(args)...);
-    }
-}
-
-template <typename BidirIterator, typename Tuple, std::size_t Index = 0> 
-constexpr void tuple2arr(BidirIterator ite, const Tuple &tpl) {
-    if constexpr (Index == std::tuple_size<Tuple>::value) {
-        return;
-    } else {
-        *ite = std::get<Index>(tpl);
-        tuple2arr<BidirIterator, Tuple, Index + 1>(std::next(ite), tpl);
-    }
-}
-
-template <typename T, std::size_t Size>
-struct appender {
-    T t;
-    ll base;
-    const std::array<ll, Size> &mods;
-
-    appender(T t, ll base, const std::array<ll, Size> &mods) : t(t), base(base), mods(mods) { }
-    appender(ll base, const std::array<ll, Size> &mods) : appender(T(), base, mods) { }
-
-    template <typename ...Args>
-    auto append(Args&&... args) const {
-        static_assert(sizeof...(args) == Size);
-        std::array<ll, Size> arr = {{ args... }};
-        for (ll i = 0; i < Size; i++) {
-            arr[i] *= base;
-            arr[i] += t;
-            arr[i] %= mods[i];
-        }
-        return arr2tuple<Size, 0>(arr.rbegin());
-    }
-
-    template <typename ...Args>
-    auto operator ()(Args&&... args) const {
-        return append(std::forward<Args>(args)...);
-    }
-};
-
-template <typename T, std::size_t Size>
-typename sized_tuple<T, Size>::type init_tuple(T init_v) {
-    std::array<T, Size> arr;
-    for (ll i = 0; i < Size; i++) arr[i] = init_v;
-    return arr2tuple<Size, 0>(arr.rbegin());
-}
-
-}  // anonymous
-
-template <ll ...Mods>
+template <ll Base, ll ...Mods>
 struct RollingHash {
-    using data_t = typename sized_tuple<ll, sizeof...(Mods)>::type;
+    constexpr static std::size_t mods_sz = sizeof...(Mods);
+    
+    using data_t = typename std::array<ll, mods_sz>; 
 
+    constexpr static data_t mods = {{ Mods... }};
     data_t dat;
     std::size_t sz;
 
     RollingHash() : dat(), sz(0) { }
-    RollingHash(data_t &&dat, std::size_t sz) : dat(std::move(dat)), sz(sz) { }
     RollingHash(const data_t &dat, std::size_t sz) : dat(dat), sz(sz) { }
+
+    RollingHash append(ll e) {
+        data_t nxt;
+        for (ll i = 0; i < mods_sz; i++) {
+            ll mod = mods[i];
+            ll v = dat[i] * Base % mod + e % mod;
+            if (mod <= v) v -= mod;
+            nxt[i] = v;
+        }
+
+        return RollingHash(nxt, sz + 1);
+    }
 
     bool operator ==(const RollingHash &oth) const {
         return sz == oth.sz && dat == oth.dat;
+    }
+
+    bool operator !=(const RollingHash &oth) const {
+        return !(*this == oth);
+    }
+
+    ll operator [](std::size_t idx) const {
+        return dat[idx];
+    }
+
+    ll& operator [](std::size_t idx) {
+        return dat[idx];
+    }
+};
+
+template <ll Base, ll ...Mods>
+struct BasePows {
+    constexpr static std::size_t mods_sz = sizeof...(Mods);
+    constexpr static std::array<ll, mods_sz> mods = {{ Mods... }};
+
+    std::array<ll, sizeof...(Mods)> pows;
+
+    BasePows() {
+        std::fill(ALL(pows), 1);
+    }
+
+    BasePows next() {
+        BasePows<Base, Mods...> ret;
+        for (ll i = 0; i < mods_sz; i++) ret[i] = pows[i] * Base % mods[i];
+        return ret;
+    }
+
+    ll operator [](std::size_t idx) const {
+        return pows[idx];
+    }
+
+    ll& operator [](std::size_t idx) {
+        return pows[idx];
     }
 };
 
 template <ll ...Mods>
 struct RollingHashBuilder {
-    using hash_t = RollingHash<Mods...>;
-    using data_t = typename hash_t::data_t;
-
     constexpr static ll base = 1007;
+    
+    using hash_t = RollingHash<base, Mods...>;
+    using data_t = typename hash_t::data_t;
+    using pows_t = BasePows<base, Mods...>;
+
     constexpr static std::size_t mods_sz = sizeof...(Mods);
     constexpr static std::array<ll, mods_sz> mods = {{ Mods... }};
-    vec<data_t> pows;
+    vec<pows_t> pows;
+
+    RollingHashBuilder() {
+        pows.push_back(pows_t());
+    }
     
-    void append_pows(std::size_t sz) {
-        static const appender<ll, mods_sz> app(base, mods);
-        if (pows.empty()) pows.push_back(init_tuple<ll, mods_sz>(1));
+    void repl_pows(std::size_t sz) {
         for (ll i = pows.size(); i <= sz; i++) {
-            data_t arg = pows.back();
-            data_t tmp = std::apply(app, arg);
-            pows.push_back(tmp);
+            auto nxt = std::move(pows.back().next());
+            pows.push_back(std::move(nxt));
         }
     }
 
     template <typename BidirIterator>
     vec<hash_t> build(BidirIterator first, BidirIterator last) {
         auto sz = std::distance(first, last);
-        append_pows(sz + 1);
         vec<hash_t> ret(sz + 1);
         auto ite = first;
-        for (ll i = 0; i < sz; i++) {
-            appender<ll, mods_sz> app(*ite, base, mods);
-            auto dat = std::apply(app, ret[i].dat);
-            ret[i + 1] = hash_t(dat, i + 1);
-            std::advance(ite, 1);
+        ll idx = 0;
+        for (auto ite = first; ite != last; ite = std::next(ite), idx++) {
+            auto e = *ite;
+            ret[idx + 1] = ret[idx].append(static_cast<ll>(e));
         }
         return ret;
     }
 
     // [l, r)
     hash_t substr(const vec<hash_t> &hash, ll l, ll r) {
-        append_pows(r - l + 1);
-        std::array<ll, mods_sz> hl, hr, ps, ans;
-        tuple2arr(hl.begin(), hash[l].dat);
-        tuple2arr(hr.begin(), hash[r].dat);
-        tuple2arr(ps.begin(), pows[r - l]);
+        repl_pows(r - l + 1);
+        const auto &hl = hash[l];
+        const auto &hr = hash[r];
+        const auto &ps = pows[r - l];
+        data_t ans;
         for (ll i = 0; i < mods_sz; i++) {
             ll mod = mods[i];
             ans[i] = hr[i] - hl[i] * ps[i] % mod + mod;
             if (mod <= ans[i]) ans[i] -= mod;
         }
-        return hash_t(arr2tuple<mods_sz, 0>(ans.rbegin()), r - l);
+        return hash_t(ans, r - l);
     }
 
     hash_t concat(const hash_t &d1, const hash_t &d2) {
-        append_pows(d2.sz + 1);
-        std::array<ll, mods_sz> h1, h2, ps, ans;
-        tuple2arr(h1.begin(), d1.dat);
-        tuple2arr(h2.begin(), d2.dat);
-        tuple2arr(ps.begin(), pows[d2.sz]);
+        repl_pows(d2.sz + 1);
+        const auto &h1 = d1;
+        const auto &h2 = d2;
+        const auto &ps = pows[d2.sz];
+        data_t ans;
         for (ll i = 0; i < mods_sz; i++) {
             ll mod = mods[i];
             ans[i] = h1[i] * ps[i] % mod + h2[i];
             if (mod <= ans[i]) ans[i] -= mod;
         }
-        return hash_t(arr2tuple<mods_sz, 0>(ans.rbegin()), d1.sz + d2.sz);
+        return hash_t(ans, d1.sz + d2.sz);
     }
 };
 
