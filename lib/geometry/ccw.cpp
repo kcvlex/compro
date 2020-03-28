22741d5162e469c8bf29ec2cdaf722cd12de8328
kcvlex <krkr9893@gmail.com>
2020-03-28 09:31:14 +0900

    split files

---
+++ lib/geometry/ccw.cpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "base.cpp"
+
+namespace geo {
+
+/*      
+ * Point c is
+ *
+ *         1
+ * -----------------
+ *  -2  a  0  b  2
+ * -----------------
+ *        -1
+ */
+
+int ccw(Point a, Point b, Point c) {
+    b -= a;
+    c -= a;
+    double p, q;
+    {
+        auto tmp = std::conj(b) * c;
+        p = tmp.real();
+        q = tmp.imag();
+    }
+    if (0 < q) return 1;
+    if (q < 0) return -1;
+    if (p < 0) return 2;
+    if (std::norm(b) < std::norm(c)) return -2;
+    return 0;
+}
+
+}
---
+++ lib/geometry/cross.cpp
@@ -0,0 +1,85 @@
+#pragma once
+#include "base.cpp"
+#include "ccw.cpp"
+
+namespace geo {
+
+bool intersect(const Line &p, const Line &q) {
+    if (!is_parallel(p, q)) return true;
+    return is_same_line(p[0], p[1], q[1]);
+}
+
+bool intersect(const Line &l, const Seg &s) {
+    double a = cross(l[1] - l[0], s[0] - l[0]);
+    double b = cross(l[1] - l[0], s[1] - l[0]);
+    return a * b < 0;
+}
+
+bool intersect(const Seg &p, const Seg &q) {
+    bool pf = (ccw(p[0], p[1], q[0]) * ccw(p[0], p[1], q[1])) <= 0;
+    bool qf = (ccw(q[0], q[1], p[0]) * ccw(q[0], q[1], p[1])) <= 0;
+    return pf && qf;
+}
+
+bool intersect(const Line &l, const Point &p) {
+    return is_same_line(p, l[0], l[1]);
+}
+
+bool intersect(const Seg &s, const Point &p) {
+    double l1 = std::abs(s[0] - s[1]);
+    double l2 = std::abs(s[0] - p);
+    double l3 = std::abs(s[1] - p);
+    return is_zero(l2 + l3 - l1);
+}
+
+double distance(const Point &p1, const Point &p2) {
+    return std::abs(p1 - p2);
+}
+
+double distance(const Line &l, const Point &p) {
+    return std::abs(p - proj(l, p));
+}
+
+double distance(const Line &p, const Line &q) {
+    if (is_parallel(p, q)) return distance(p, q[0]);
+    return 0;
+}
+
+double distance(const Line &l, const Seg &s) {
+    if (intersect(l, s)) return 0;
+    return std::min(distance(l, s[0]), distance(l, s[1]));
+}
+
+double distance(const Seg &s, const Point &p) {
+    Line l(s[0], s[1]);
+    Point pr = proj(l, p);
+    if (intersect(s, pr)) return std::abs(pr - p);
+    return std::min(std::abs(s[0] - p), std::abs(s[1] - p));
+}
+
+double distance(const Seg &p, const Seg &q) {
+    if (intersect(p, q)) return 0;
+    return var_min(distance(p, q[0]), distance(p, q[1]),
+                   distance(q, p[0]), distance(q, p[1]));
+}
+
+int intersect(const Circle &c1, const Circle &c2) {
+    if (c1.r < c2.r) return intersect(c2, c1);
+    double d = distance(c1.p, c2.p);
+    if (d + c2.r < c1.r) return 1;
+    if (eq(d + c2.r, c1.r)) return 2;
+    if (d < c1.r + c2.r) return 3;
+    if (eq(c1.r - c2.r, d)) return 4;
+    return 0;
+}
+
+std::pair<Point, Point> crosspoint(const Circle &c1, const Circle &c2) {
+    double d = distance(c1.p, c2.p);
+    double rc = cos(d, c1.r, c2.r) * c1.r;
+    double rs = std::sqrt(c1.r * c1.r - rc * rc);
+    auto unit_v = (c2.p - c1.p) / d;
+    return std::make_pair(c1.p + unit_v * Point(rc, rs),
+                          c1.p + unit_v * Point(rc, -rs));
+}
+
+}
