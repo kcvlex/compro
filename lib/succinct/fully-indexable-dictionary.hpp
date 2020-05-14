a9fac2d88cba1767322596a9a659d4a64872288a
kcvlex <krkr9893@gmail.com>
2020-05-14 17:14:37 +0900

    WIP

--- lib/succinct/fully-indexable-dictionary.hpp
+++ lib/succinct/fully-indexable-dictionary.hpp
@@ -15,29 +15,23 @@ private:
     constexpr static size_type block_sz = sz_log2 / 2;
     constexpr static size_type block_per_chunk = chunk_sz / block_sz;
 
-    size_type sz;
+    size_type sz, cnt1;
     vec<size_type> dat;
     vec<size_type> chunk;
     vvec<size_type> block;
 
-    size_type dat_idx(size_type i, size_type j) const {
-        return i * block_per_chunk + j;
-    }
-
 public:
-    
     FullyIndexableDictionaly() { } 
 
     template <typename F>
     FullyIndexableDictionaly(F f, size_type sz) 
-        : sz(sz), 
+        : sz(sz), cnt1(0),
           dat(ceil_div(sz, block_sz), 0), 
           chunk(ceil_div(sz, chunk_sz)), 
           block(make_v<block_type>(chunk.size(), block_per_chunk))
     {
-        size_tupe sum = 0;
         for (size_type i = 0; i < chunk.size(); i++) {
-            chunk[i] = sum;
+            chunk[i] = cnt1;
             size_type tmp = 0;
             for (size_type j = 0; j < block[i].size(); j++) {
                 block[i][j] = tmp;
@@ -45,30 +39,31 @@ public:
                     size_type idx = i * chunk_sz + j * block_sz;
                     auto v = f(idx + k);
                     size_type mask = (1ll << k) * v;
-                    dat[dat_idx(i, j)] |= mask;
+                    dat[i * block_per_chunk + j] |= mask;
                     tmp += v;
                 }
                 block[i][j] = tmp;
             }
-            sum += tmp;
+            cnt1 += tmp;
         }
     }
 
     size_type rank(bool b, size_type pos) const {
         if (!b) return pos - rank(!b, pos);
-        size_type ret = 0, store = pos;
+        size_type ret = 0, idx = 0;
         
         size_type i = pos / chunk_sz;
         pos %= chunk_sz;
+        idx += i * chunk_sz * block_per_chunk;
         ret += chunk[i];
         
         size_type j = pos / block_sz;
         pos %= block_sz;
+        idx += j * block_sz;
         ret += block[i][j];
         
-        size_type rest = store - pos;
         auto mask = (1ll << rest) - 1;
-        return ret + utility::popcount(dat[dat_idx(i, j)] & mask);
+        return ret + utility::popcount(dat[idx] & mask);
     }
 
     size_type rank(bool b, size_type l, size_typr r) const {
@@ -89,6 +84,10 @@ public:
     bool operator [](size_type i) const {
         size_type offset = i % block_sz;
         return (dat[i / block_sz] >> offset) & 1;
+    }
+
+    size_type sum(bool b) const {
+        return b ? cnt1 : sz - cnt1;
     }
 };
 
--- lib/succinct/wavelet-matrix.hpp
+++ lib/succinct/wavelet-matrix.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "fully-indexable-dictionaly.hpp"
+#include "fully-indexable-dictionary.hpp"
 
 namespace succinct { 
 
@@ -8,11 +8,39 @@ struct WaveletMatrix {
     using size_type = data_type::size_type;
 
 private:
-    using data_type = FullyIndexableDictionaly<MaxSize>;
+    using data_type = FullyIndexableDictionary<MaxSize>;
 
     size_type sz;
     std::array<data_type, MaxValueLog> mat;
-    std::array<size_type, MaxSizeLog> cnt0;
+    std::array<vec<size_type>, MaxValueLog> acc_sum;
+
+    void update_range_aux(bool bt, size_type &l, size_type &r, size_type i) const {
+        const auto &dic = mat[i];
+        r = dic.rank(bt, l, r) + bt * dic.sum(0);
+        l = dic.rank(bt, l) + bt * dic.sum(0);
+    }
+
+    size_type range_freq_aux(size_type l, size_type r, T bound) const {
+        size_type ret = 0;
+        for (size_type i = 0; i < MaxValueLog && l < r; i++) {
+            T mask = 1ll << (MaxValueLog - (i + 1));
+            bool bt = !!(t & mask);
+            if (bt) ret += dic.rank(0, l, r);
+            update_range_aux(bt, l, r, i);
+        }
+        return ret;
+    }
+
+    size_type range_sum_aux(size_type l, size_type r, T bound) const {
+        T ret = 0;
+        for (size_type i = 0; i < MaxValueLog && l < r; i++) {
+            T mask = 1ll << (MaxValueLog - (i + 1));
+            bool bt = !!(t & mask);
+            if (bt) ret += acc_sum[i][r] - acc_sum[i][l];
+            update_range_aux(bt, l, r, i);
+        }
+        return ret;
+    }
 
 public:
     WaveletMatrix(vec<T> dat) : sz(dat.size()) {
@@ -25,11 +53,19 @@ public:
             mat[i] = std::move(data_type(f, dat.size()));
             size_type mask = 1ll << (MaxValueLog - i - 1);
             size_type l = 0, r = 0;
-            for (auto e : dat) {
-                if (e & mask) buf[sz - (++r)] = e;
-                else buf[l++] = e;
+            acc_sum[i].resize(sz + 1);
+            for (size_type j = 0; j < dat.size(); j++) {
+                auto e = dat[j];
+                if (e & mask) {
+                    r++;
+                    buf[sz - r] = e;
+                } else {
+                    buf[l] = e;
+                    acc_sum[i][j] += e;
+                    l++;
+                }
             }
-            cnt0[i] = l;
+            for (size_type k = 0; k < dat.size(); k++) acc_sum[i][k + 1] += acc_sum[i][k];
             std::reverse(buf.begin() + l, buf.end());
             std::swap(buf, dat);
         }
@@ -39,11 +75,8 @@ public:
         size_type mask = 1ll << (MaxValueLog - 1);
         size_type l = 0, r = pos;
         for (size_type i = 0; i < MaxValueLog; i++, mask /= 2) {
-            const auto &dic = mat[i];
             bool bt = !!(t & mask);
-            size_type nl = dic.rank(bt, l) + bt * cnt0[i];
-            size_type nr = dic.rank(bt, l, r) + bt * cnt0[i];
-            l = nl, r = nr;
+            update_range_aux(bt, l, r, i);
         }
         return r - l;
     }
@@ -53,11 +86,8 @@ public:
         size_type l = 0, r = pos;
         for (size_type i = 0; i < MaxValueLog; i++, mask /= 2) {
             larr[i] = l, rarr[i] = r;
-            const auto &dic = mat[i];
             bool bt = !!(t & mask);
-            size_type nl = dic.rank(bt, l) + bt * cnt0[i];
-            size_type nr = dic.rank(bt, r) - dic.rank(bt, l) + bt * cnt0[i];
-            l = nl, r = nr;
+            update_range_aux(bt, l, r, i);
         }
 
         for (size_type i = 0, mask = 1; i < MaxValueLog; i++, mask *= 2) {
@@ -80,18 +110,25 @@ public:
             const auto &dic = mat[i];
             size_type cz = dic.rank(0, l, r);
             if (k < cz) {
-                size_type nl = dic.rank(0, l);
-                size_type nr = dic.rank(0, l, r);
-                l = nl, r = nr;
+                r = dic.rank(0, l, r);
+                l = dic.rank(0, l);
             } else {
                 k -= cz;
                 ret += mask;
-                size_type nl = dic.rank(1, l) + cnt0[i];
-                size_type nr = dic.rank(1, l, r) + cnt0[i];
-                l = nl, r = nr;
+                r = dic.rank(1, l, r) + dic.sum(0);
+                l = dic.rank(1, l) + dic.sum(0);
             }
         }
         return ret;
+    }
+
+    // [l, r), [min, max)
+    size_type range_freq(size_type l, size_type r, T min, T max) const {
+        return range_freq_aux(l, r, max) - range_freq_aux(l, r, min);
+    }
+
+    size_type range_sum(size_type l, size_type r, T min, T max) const {
+        return range_sum_aux(l, r, max) - range_sum_aux(l, r, min);
     }
 };
 
