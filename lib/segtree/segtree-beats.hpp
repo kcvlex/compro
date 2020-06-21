44485742c49e0240e982ed0aa2b9c084789253e4
kcvlex <krkr9893@gmail.com>
2020-06-21 23:49:13 +0900

    beats

--- lib/segtree/segtree-beats.hpp
+++ lib/segtree/segtree-beats.hpp
@@ -1,186 +1,229 @@
 #include "../util/template.hpp"
 #include "../util/bit-op.hpp"
-#include "../util/monoid-validator.hpp"
-#include "/home/taroy/kyopuro/lib/util/debug.hpp"
 
 namespace segtree {
 
 namespace nodes {
 
-template <typename T, typename Comp>
+template <typename T, typename Comp, T INF>
 struct segment {
+    static_assert(std::is_integral<T>::value, "T must be integer");
+
     using size_type = ssize_t;
-    T min1, min2, min_lazy; // comp(min1, min2) is true
-    T max1, max2, max_lazy; // comp(max2, max1) is true
-    T sum;
-    size_type max_cnt = 0, min_cnt = 0;
-    bool has_max_lazy = false, has_min_lazy = false;
+    T min1, min2; // comp(min1, min2) is true
+    T max1, max2; // comp(max2, max1) is true
+    T sum, add = 0;
+    size_type max_cnt = 0, min_cnt = 0, l, r;
 
-    segment(T min1, T min2, T max1, T max2, T sum) 
-        : min1(min1), min2(min2), min_lazy(T()), 
-          max1(max1), max2(max2), max_lazy(T()), sum(sum) { }
-
-    segment() : segment(T(), T(), T(), T()) { }
-
-    void update_lazy_max(const T &v) noexcept {
-        apply();
-        if (comp(max1, v)) return;
-        has_max_lazy = true;
-        max_lazy = v;
+    segment(T min1, T min2, T max1, T max2, T sum, size_type l, size_type r, bool enable_) 
+        : min1(min1), min2(min2),
+          max1(max1), max2(max2),
+          sum(sum), l(l), r(r), enable_(enable_)
+    {
     }
 
-    void update_lazy_min(const T & v) noexcept {
-        apply();
-        if (comp(v, min1)) return;
-        has_min_lazy = true;
-        min_lazy = v;
+    segment(T min1, T min2, T max1, T max2, T sum, size_type l, size_type r)
+        : segment(min1, min2, max1, max2, sum, l, r, true)
+    {
     }
 
-    void apply() noexcept {
-        apply_min();
-        apply_max();
-        update_minmax();
+    segment(T v, size_type l, size_type r)
+        : segment(v, INF, v, -INF, v, l, r)
+    {
+        max_cnt = min_cnt = 1;
+    }
+
+    segment() : segment(T(), T(), T(), T(), T(), -1, -1, false) { }
+
+    // caller is chmax
+    void update_min(const T &v) noexcept {
+        sum -= min1 * min_cnt;
+        sum += v * min_cnt;
+        if (min1 == max1) {
+            min1 = max1 = v;
+        } else if (min1 == max2) {
+            min1 = max2 = v;
+        } else {
+            min1 = v;
+        }
+    }
+    
+    // caller is chmin
+    void update_max(const T &v) noexcept {
+        sum -= max1 * max_cnt;
+        sum += v * max_cnt;
+        if (max1 == min1) {
+            max1 = min1 = v;
+        } else if (max1 == min2) {
+            max1 = min2 = v;
+        } else {
+            max1 = v;
+        }
+    }
+
+    void update_add(const T &v) noexcept {
+        sum += (r - l) * v;
+        min1 += v;
+        max1 += v;
+        if (min2 != INF) min2 += v;
+        if (max2 != -INF) max2 += v;
+        add += v;
+    }
+
+    bool enable() const noexcept {
+        return enable_;
     }
 
     static segment merge(const segment &s1, const segment &s2) {
-        constexpr size_type sz = 4;
-        T min_lis[] = { s1.min1, s1.min2, s2.min1, s2.min2, };
-        T max_lis[] = { s1.max1, s1.max2, s2.max1, s2.max2, };
         Comp c;
-        std::sort(min_lis, min_lis + 4, c);
-        std::unique(min_lis, min_lis + 4);
-        std::sort(max_lis, max_lis + 4, [&](const T &a, const T &b) { return !c(a, b); });
-        std::unique(max_lis, max_lis + 4);
-        segment ret(min_lis[0], min_lis[1], max_lis[0], max_lis[1], s1.sum + s2.sum);
-        for (auto ptr : { &s1, &s2 }) {
-            if (ptr->min1 == ret.min1) ret.min_cnt += ptr->min_cnt;
-            if (ptr->max1 == ret.max1) ret.max_cnt += ptr->max_cnt;
-        }
+        auto min = [&](const T &a, const T &b) { return c(a, b) ? a : b; };
+        auto max = [&](const T &a, const T &b) { return c(a, b) ? b : a; };
+
+        T min1 = min(s1.min1, s2.min1);
+        T min2 = (c(s1.min1, s2.min1) ? min(s1.min2, s2.min1) :  // s1.min1 < s2.min1
+                  c(s2.min1, s1.min1) ? min(s1.min1, s2.min2) :  // s1.min1 > s2.min1
+                                        min(s1.min2, s2.min2));
+        
+        T max1 = max(s1.max1, s2.max1);
+        T max2 = (c(s1.max1, s2.max1) ? max(s1.max1, s2.max2) :  // s1.max1 < s2.max1
+                  c(s2.max1, s1.max1) ? max(s1.max2, s2.max1) :  // s1.max1 > s2.max1
+                                        max(s1.max2, s2.max2));
+
+        size_type l = std::min(s1.l, s2.l);
+        size_type r = std::max(s1.r, s2.r);
+        segment ret(min1, min2, max1, max2, s1.sum + s2.sum, l, r);
+        if (s1.min1 == ret.min1) ret.min_cnt += s1.min_cnt;
+        if (s1.max1 == ret.max1) ret.max_cnt += s1.max_cnt;
+        if (s2.min1 == ret.min1) ret.min_cnt += s2.min_cnt;
+        if (s2.max1 == ret.max1) ret.max_cnt += s2.max_cnt;
+        return ret;
+    }
+
+    std::string to_string() const {
+        std::stringstream ss;
+        ss << "(" << l << ", " << r << "), "
+           << "(" << min1 << ", " << min2 << ", " << min_cnt << "), "
+           << "(" << max1 << ", " << max2 << ", " << max_cnt << "), "
+           << sum << ", " << add << "\n";
+        std::string ret;
+        std::getline(ss, ret);
         return ret;
     }
 
 private:
-    // min1 < min_lazy < min2
-    void apply_min() noexcept {
-        if (!has_lazy_min) return;
-        has_lazy_min = false;
-        sum += lazy_min * min_cnt;
-        sum -= min1 * min_cnt;
-        min1 = lazy_min;
-    }
-
-    // max2 < maz_lazy < max1
-    void apply_max() noexcept {
-        if (!has_lazy_max) return;
-        has_lazy_max = false;
-        sum += lazy_max * max_cnt;
-        sum -= max1 * max_cnt;
-        max1 = lazy_max;
-    }
-
-    void update_minmax() {
-        T lis[] = { min1, min2, max1, max2, };
-        std::sort(min1, min2, max1, max2, Comp());
-        auto end = std::unique(lis, lis + 4);
-        min1 = lis[0]; min2 = lis[1];
-        max1 = *(end - 1), max2 = *(end - 2);
-    }
+    bool enable_;
 };
 
 }
 
-template <typename T, typename Comp = std::less<T>, 
-          typename Segment = nodes::segment<T, Comp>>
+template <typename T, T INF, 
+          typename Comp = std::less<T>, 
+          typename Segment = nodes::segment<T, Comp, INF>>
 struct SegmentTreeBeats {
     using size_type = ssize_t;
 
 private:
     Comp comp;
-    T id_ele, inf;
     vec<Segment> segs;
-    size_type height;
+
+    // min1 < v < min2
+    bool should_update_min(const Segment &s, const T &v) const noexcept {
+        return comp(s.min1, v) && comp(v, s.min2);
+    }
+
+    // max2 < v < max1
+    bool should_update_max(const Segment &s, const T &v) const noexcept {
+        return comp(s.max2, v) && comp(v, s.max1);
+    }
 
     void push(size_type idx) {
         auto &s = segs[idx];
-        if (!s.has_min_lazy && !s.has_max_lazy) return;
-        auto min_lazy = s.min_lazy, max_lazy = s.max_lazy;
-        bool has_min_lazy = s.has_min_lazy, has_max_lazy = s.has_max_lazy;
-        s.apply();
         for (int i = 0; i < 2; i++) {
-            size_type cidx = idx + i;
+            size_type cidx = 2 * idx + i;
             if (segs.size() <= cidx) break;
             auto &cs = segs[cidx];
-            if (has_min_lazy) cs.update_lazy_min(min_lazy);
-            if (has_max_lazy) cs.update_lazy_max(max_lazy);
+            if (!cs.enable()) continue;
+            cs.update_add(s.add);
+            if (comp(cs.min1, s.min1)) cs.update_min(s.min1);  // cs.min1 < s.min1
+            if (comp(s.max1, cs.max1)) cs.update_max(s.max1);  // s.max1 < cs.max1
         }
+        s.add = 0;
     }
 
-    void update_from_bottom(size_type idx) {
-        while (true) {
-            auto pidx = idx / 2;
-            if (pidx == 0) break;
-            auto c1 = 2 * pidx + 0;
-            auto c2 = 2 * pidx + 1;
-            push(c1); push(c2);
-            segs[idx] = merge(segs[c1], segs[c2]);
-            idx = pidx;
-        }
-    }
-
-    void propagate_from_top(size_type idx) {
-        for (int i = height; 1 <= i; i--) push(idx >> i);
-    }
-
-    size_type get_endpoint_seg_idx(size_type i) const noexcept {
-        i += size();
-        return i / (i & -i);
-    }
-
-    auto query_preprocess(size_type ql, size_type qr) -> std::pair<size_type, size_type> {
-        for (auto i : { ql, qr }) {
-            auto idx = get_endpoint_seg_idx(i);
-            propagate_from_top(i);
-        }
-        return std::make_pair(ql + size(), qr + size());
-    }
-
-    void update_query_postprocess(size_type ql, size_type qr) {
-        for (auto i : { ql, qr }) {
-            auto idx = get_endpoint_seg_idx(i);
-            update_from_bottom(i);
+    void merge(size_type idx) {
+        size_type c0 = idx * 2 + 0,
+                  c1 = idx * 2 + 1;
+        if (segs.size() <= c0 || !segs[c0].enable()) return;
+        if (segs.size() <= c1 || !segs[c1].enable()) {
+            segs[idx] = segs[c0];
+            segs[idx].add = 0;
+        } else {
+            segs[idx] = Segment::merge(segs[c0], segs[c1]);
         }
     }
 
     template <typename F>
-    void traverse_necessary_segs(size_type ql, size_type qr, F f) {
-        auto [ lnode, rnode ] = query_preprocess(ql, qr);
-        while (lnode < rnode) {
-            if (lnode & 1) {
-                push(lnode);
-                f(lnode);
-                lnode++;
-            }
-            if (rnode & 1) {
-                rnode--;
-                push(rnode);
-                f(rnode);
-            }
-            lnode /= 2, rnode /= 2;
+    void query(size_type ql, size_type qr, F f, size_type idx) {
+        if (segs.size() <= idx) return;
+        if (!segs[idx].enable()) return;
+        push(idx);
+        ll nl = segs[idx].l, nr = segs[idx].r;
+        if (ql <= nl && nr <= qr) {
+            f(idx);
+            push(idx);
+        } else if (nr <= ql || qr <= nl) {
+            // nothing
+        } else {
+            query(ql, qr, f, 2 * idx + 0);
+            query(ql, qr, f, 2 * idx + 1);
+            merge(idx);
+        }
+    }
+
+    void chmin(size_type ql, size_type qr, size_type idx, const T &t) {
+        if (segs.size() <= idx) return;
+        auto &s = segs[idx];
+        if (!s.enable()) return;
+        push(idx);
+        ll nl = s.l, nr = s.r;
+        if (nr <= ql || qr <= nl) return;
+        if (comp(s.max1, t) || s.max1 == t) return; // max1 <= t
+        if (ql <= nl && nr <= qr && should_update_max(s, t)) {
+            s.update_max(t);
+            push(idx);
+        } else {
+            chmin(ql, qr, 2 * idx + 0, t);
+            chmin(ql, qr, 2 * idx + 1, t);
+            merge(idx);
+        }
+    }
+
+    void chmax(size_type ql, size_type qr, size_type idx, const T &t) {
+        if (segs.size() <= idx) return;
+        auto &s = segs[idx];
+        if (!s.enable()) return;
+        push(idx);
+        ll nl = s.l, nr = s.r;
+        if (nr <= ql || qr <= nl) return;
+        if (comp(t, s.min1) || s.min1 == t) return; // t <= s.min1
+        if (ql <= nl && nr <= qr && should_update_min(s, t)) {
+            s.update_min(t);
+            push(idx);
+        } else {
+            chmax(ql, qr, 2 * idx + 0, t);
+            chmax(ql, qr, 2 * idx + 1, t);
+            merge(idx);
         }
     }
 
 public:
     template <typename F>
-    SegmentTreeBeats(F f, size_type sz, T id_ele, T inf)
-        : id_ele(id_ele), inf(inf),
-          segs(2 * utility::ceil_pow2(sz)),
-          height(utility::msb(segs.size() / 2))
-    {
-        for (size_type i = 0; i < sz; i++) segs[i + sz] = Segment(f(i), id_ele);
-        for (size_type i = sz - 1; 1 <= i; i--) {
-            size_type c1 = 2 * i + 0;
-            size_type c2 = 2 * i + 1;
-            segs[i] = Segment::merge(segs[c1], segs[c2]);
+    SegmentTreeBeats(F f, size_type sz) : segs(2 * utility::ceil_pow2(sz)) {
+        for (size_type i = 0; i < sz; i++) {
+            segs[segs.size() / 2 + i] = Segment(f(i), i, i + 1);
+        }
+        for (size_type i = segs.size() / 2 - 1; 1 <= i; i--) {
+            merge(i);
         }
     }
 
@@ -189,50 +232,48 @@ public:
     }
 
     void chmin(size_type ql, size_type qr, const T &t) {
-        auto f = [&](size_type i) {
-            auto &s = segs[i];
-            bool update = comp(s.max2, t) && comp(t, s.max1);  // max2 < t < max1
-            if (!update) return;
-            s.update_max(t);
-        };
-        traverse_necessary_segs(ql, qr, f);
-        update_query_postprocess(ql, qr);
+        chmin(ql, qr, 1, t);
     }
 
     void chmax(size_type ql, size_type qr, const T &t) {
-        auto f = [&](size_type i) {
-            auto &s = segs[i];
-            bool update = comp(s.min1, t) && comp(t, s.min2);  // min1 < t < min2
-            if (!update) return;
-            s.update_min(t);
-        };
-        traverse_necessary_segs(ql, qr, f);
-        update_query_postprocess(ql, qr);
+        chmax(ql, qr, 1, t);
     }
 
-    T get_sum_query(size_type ql, size_type qr) {
+    void add(size_type ql, size_type qr, const T &t) {
+        auto f = [&](size_type i) { 
+            segs[i].update_add(t); 
+        };
+        query(ql, qr, f, 1);
+    }
+
+    T get_sum(size_type ql, size_type qr) {
         T ret = T();
-        auto f = [&](size_type i) { retl += segs[i].sum; };
-        traverse_necessary_segs(ql, qr, f);
+        auto f = [&](size_type i) {
+            ret += segs[i].sum; 
+        };
+        query(ql, qr, f, 1);
+        return ret;
     }
 
     T get_min(size_type ql, size_type qr) {
-        T ret = inf;
+        T ret = INF;
         auto f = [&](size_type i) {
             auto v = segs[i].min1;
             if (comp(v, ret)) ret = v;
         };
-        traverse_necessary_segs(ql, qr, f);
+        query(ql, qr, f, 1);
         return ret;
     }
 
     T get_max(size_type ql, size_type qr) {
-        T ret = -inf;
+        T ret = -INF;
         auto f = [&](size_type i) {
             auto v = segs[i].max1;
             if (comp(ret, v)) ret = v;
         };
-        traverse_necessary_segs(ql, qr, f);
+        query(ql, qr, f, 1);
         return ret;
     }
 };
+
+}
