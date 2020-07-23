383df530394b26f43490fbf324acf748f2444b5f
kcvlex <krkr9893@gmail.com>
2020-07-24 02:41:01 +0900

    fraction

--- lib/math/fraction.hpp
+++ lib/math/fraction.hpp
@@ -4,116 +4,107 @@
 namespace math {
 
 constexpr ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+constexpr ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+constexpr ll abs(ll n) { return n < 0 ? -n : n; }
 
 struct fraction {
-    ll num, den;  // WARNING : den == 0
-    bool sgn_;  // 0 : plus, 1 : minus
+    ll num, den;
+    bool neg;
 
-    fraction(ll n, ll d) {
-        sgn_ = (n * d < 0);
-        n = std::abs(n);
-        d = std::abs(d);
-        if (n && d) {
-            ll g = gcd(n, d);
-            num = n / g;
-            den = d / g;
-        } else {
-            num = 0, den = 0;
-            if (n) num = n / n;
-            if (d) den = d / d;
+    constexpr fraction(ll num, ll den)
+        : fraction(abs(num), abs(den), gcd(abs(num), abs(den)), (num < 0) ^ (den < 0)) { }
+
+    constexpr fraction(ll num_) : fraction(num_, 1) { }
+
+    constexpr fraction() : fraction(0, 1) { }
+
+    constexpr fraction inv() const {
+        assert(num);
+        return fraction(den, num, 1, neg);
+    }
+
+    constexpr fraction operator-() const {
+        auto ret = *this;
+        ret.neg ^= true;
+        return ret;
+    }
+
+    constexpr fraction& operator+=(const fraction &rhs) {
+        ll l = lcm(den, rhs.den);
+        ll a = num * (l / den);
+        ll b = rhs.num * (l / rhs.den);
+        if (neg) a *= -1;
+        if (rhs.neg) b *= -1;
+        den = l;
+        num = a + b;
+        neg = (num < 0);
+        num = abs(num);
+        reduction();
+        return *this;
+    }
+
+    constexpr fraction& operator-=(const fraction &rhs) {
+        return (*this) += (-rhs);
+    }
+
+    constexpr fraction& operator*=(const fraction &rhs) {
+        neg ^= rhs.neg;
+        num *= rhs.num;
+        den *= rhs.den;
+        reduction();
+        return *this;
+    }
+
+    constexpr fraction& operator/=(const fraction &rhs) {
+        return (*this) *= rhs.inv();
+    }
+
+    constexpr bool operator==(const fraction &rhs) const {
+        return num == rhs.num &&
+               den == rhs.den &&
+               neg == rhs.neg;
+    }
+
+    constexpr bool operator<(const fraction &rhs) const {
+        ll lv = num * rhs.den;
+        ll rv = rhs.num * den;
+        if (neg) lv *= -1;
+        if (rhs.neg) rv *= -1;
+        return lv < rv;
+    }
+
+    constexpr fraction operator+(const fraction &rhs) const { return fraction(*this) += rhs; }
+    constexpr fraction operator-(const fraction &rhs) const { return fraction(*this) -= rhs; }
+    constexpr fraction operator*(const fraction &rhs) const { return fraction(*this) *= rhs; }
+    constexpr fraction operator/(const fraction &rhs) const { return fraction(*this) /= rhs; }
+    constexpr bool operator!=(const fraction &rhs) const { return !((*this) == rhs); }
+    constexpr bool operator<=(const fraction &rhs) const { return (*this) == rhs || (*this) < rhs; }
+    constexpr bool operator>=(const fraction &rhs) const { return !((*this) < rhs); }
+    constexpr bool operator>(const fraction &rhs) const { return !((*this) <= rhs); }
+
+private:
+    constexpr void reduction() {
+        if (num == 0) {
+            neg = false;
+            den = 1;
+            return;
+        }
+        auto g = gcd(num, den);
+        if (g) {
+            num /= g; 
+            den /= g;
         }
     }
 
-    constexpr int sgn() const {
-        return sgn_ ? -1 : 1;
-    }
-
-    constexpr bool operator ==(const fraction &oth) const {
-        return num == oth.num && den == oth.den && sgn() == oth.sgn();
-    }
-
-    constexpr bool operator !=(const fraction &oth) const {
-        return !(*this == oth);
-    }
-
-    constexpr bool operator <(const fraction &oth) const {
-        ll a = num * oth.den;
-        ll b = oth.num * den;
-        return a * sgn() < b * oth.sgn();
-    }
-
-    constexpr bool operator <=(const fraction &oth) const {
-        return *this == oth || *this < oth;
-    }
-
-    constexpr bool operator >(const fraction &oth) const {
-        return !(*this <= oth);
-    }
-
-    constexpr bool operator >=(const fraction &oth) const {
-        return !(*this < oth);
-    }
-
-    constexpr long double value() const {
-        return static_cast<long double>(num) / static_cast<long double>(den) * sgn();
-    }
-
-    fraction& inv() {
-        std::swap(num, den);
-        return *this;
-    }
-
-    fraction inv() const {
-        return fraction(*this).inv();
-    }
-
-    constexpr fraction& operator -() {
-        sgn_ = !sgn_;
-        return *this;
-    }
-
-    fraction operator -() const {
-        return -fraction(*this);
-    }
-
-    fraction& operator +=(const fraction &oth) {
-        ll g = gcd(den, oth.den);
-        ll n = oth.den / g * num * sgn() +
-               den / g * oth.num * oth.sgn();
-        *this = fraction(n, den / g * oth.den);
-        return *this;
-    }
-
-    fraction& operator -=(const fraction &oth) {
-        return (*this) += (-oth);
-    }
-
-    fraction& operator *=(const fraction &oth) {
-        ll n = num * oth.num * sgn() * oth.sgn();
-        ll d = den * oth.den;
-        *this = fraction(n, d);
-        return *this;
-    }
-
-    fraction& operator /=(const fraction &oth) {
-        return (*this) *= oth.inv();
-    }
-
-    fraction operator +(const fraction &oth) const {
-        return fraction(*this) += oth;
-    }
-
-    fraction operator -(const fraction &oth) const {
-        return fraction(*this) -= oth;
-    }
-
-    fraction operator *(const fraction &oth) const {
-        return fraction(*this) *= oth;
-    }
-
-    fraction operator /(const fraction &oth) const {
-        return fraction(*this) /= oth;
-    }
+    constexpr fraction(ll num, ll den, ll g, bool neg) : num(num / g), den(den / g), neg(neg) { }
 };
+
+std::ostream& operator<<(std::ostream &os, fraction f) {
+    if (f.neg) os << "-";
+    os << f.num;
+    os << "/";
+    os << f.den;
+    return os;
+}
 
 }
