ac29f0d5f98fac4f04c73dcfa54f3dc7334dc16a
kcvlex <krkr9893@gmail.com>
2020-10-24 19:18:55 +0900

    sa, lcp (too slow...)

--- lib/strings/lcp-array.hpp
+++ lib/strings/lcp-array.hpp
@@ -3,35 +3,37 @@
 
 namespace strings {
 
+template <typename Container>
 struct LongestCommonPrefixArray {
-    const SuffixArray &sa;
-    vec<ll> lcp, rank;
+    const SuffixArray<Container> &sa;
+    vec<size_type> lcp, rank;
 
     void build() {
-        for (ll i = 0; i < sa.size(); i++) rank[sa[i]] = i;
-        ll h = 0;
-        const auto &str = sa.str();
-        for (ll i = 0; i < sa.size(); i++) {
-            if (rank[i] + 1 == sa.size()) continue;
-            ll j = sa[rank[i] + 1];
-            while (true) {
-                if (str.size() <= i + h) break;
-                if (str.size() <= j + h) break;
-                if (str[i + h] != str[j + h]) break;
-                h++;
+        for (size_type i = 0; i < sa.size(); i++) rank[sa[i]] = i;
+        size_type h = 0;
+        decltype(auto) str = sa.raw();
+        for (size_type i = 0; i <= sa.size(); i++) {
+            if (rank[i]) {
+                size_type j = sa[rank[i] - 1];
+                while (true) {
+                    if (str.size() <= i + h) break;
+                    if (str.size() <= j + h) break;
+                    if (str[i + h] != str[j + h]) break;
+                    h++;
+                }
+                lcp[rank[i]] = h;
             }
-            lcp[rank[i] + 1] = h;
-            h = std::max<ll>(0, h - 1);
+            h = std::max<size_type>(0, h - 1);
         }
     }
 
-    LongestCommonPrefixArray(const SuffixArray &sa)
-        : sa(sa), lcp(sa.size()), rank(sa.size())
+    LongestCommonPrefixArray(const SuffixArray<Container> &sa)
+        : sa(sa), lcp(sa.size() + 1), rank(sa.size() + 1)
     {
         build();
     }
 
-    ll operator [](std::size_t i) const {
+    size_type operator [](size_type i) const {
         return lcp[i];
     }
 };
--- lib/strings/suffix-array.hpp
+++ lib/strings/suffix-array.hpp
@@ -1,74 +1,75 @@
 #pragma once
-#include "../util/template.hpp"
+#include "util/template.hpp"
 
 namespace strings {
 
 template <typename Container,
           size_type Min = 0,
-          size_type Max = std::numeric_limits<char>::max() + 1>
+          size_type Max = static_cast<size_type>(std::numeric_limits<char>::max()) + 1>
 class SuffixArray {
     const Container &s;
     vec<size_type> ord2idx;
-    const size_type range = Max - Min;
 
     vec<size_type> build() {
         const size_type sz = s.size() + 1;
+        const size_type range = Max - Min;
         vec<size_type> idx2class(sz), ord2idx(sz), cnts(std::max(range, sz) + 1);
         const size_type csz = cnts.size();
 
-        auto get = [&](size_type i) {
+        auto get = [&](size_type i) -> size_type {
             if (i < sz) return s[i];
             return Min;  // i == sz;
         };
-        
-        for (size_type i = 0; i < sz; i++) cnts[get(i)]++;
-        for (size_type i = 0; i + 1 < csz; i++) cnts[i + 1] += cnts[i];
-        for (size_type i = sz - 1; 0 <= i; i--) ord2idx[--cnts[get(i)]] = i;
 
+        std::iota(ALL(ord2idx), 0ll);
+        std::sort(ALL(ord2idx), [&](const auto i, const auto j) {
+            auto c1 = get(i), c2 = get(j);
+            return c1 < c2;
+        });
+
+        std::iota(ALL(cnts), 0ll);
         idx2class[ord2idx[0]] = 0;
         for (size_type i = 1; i < sz; i++) {
             size_type i1 = ord2idx[i - 1],
                       i2 = ord2idx[i];
-            idx2class[i2] = idx2class[i1];
-
-            size_type v1 = get(ord2idx[i - 1]),
-                      v2 = get(ord2idx[i]);
-            if (v1 != v2) idx2class[i2]++;
+            if (get(i1) != get(i2)) {
+                idx2class[i2] = i;
+            } else {
+                idx2class[i2] = idx2class[i1];
+            }
         }
-            
-        auto make = [&](size_type i) {
-            const size_type a = idx2class[i];
-            size_type ni = i + (1ll << k);
-            if (sz <= ni) ni -= sz;
-            const size_type b = idx2class[ni];
-            return std::make_pair(a, b);
-        };
 
-        vec<size_type> next_idx2class(sz), next_ord2idx(sz);
+        vec<size_type> buf(sz);
         auto build_step = [&](size_type k) {
-            for (size_type i = 0; i < sz; i++) {
-                auto &v = next_ord2idx[i];
-                v = ord2idx[i] - (1ll << i);
-                if (v < 0) v += sz;
-            }
+            size_type len = 1ll << k;
 
-            std::fill(ALL(cnts), 0);
-            for (size_type i = 0; i < sz; i++) cnts[idx2class[i]]++;
-            for (size_type i = 0; i + 1 < csz; i++) cnts[i + 1] += cnts[i];
-            for (size_type i = sz - 1; 0 <= i; i--) {
-                const size_type idx = next_ord2idx[i];
-                const size_type cls = idx2class[idx];
-                ord2idx[--cnts[cls]] = idx;
-            }
+            std::swap(buf, idx2class);
 
-            next_idx2class[ord2idx[0]] = 0;
+            // build classes
+            idx2class[ord2idx[0]] = 0;
             for (size_type i = 1; i < sz; i++) {
-                size_type i1 = ord2idx[i - 1],
-                          i2 = ord2idx[i];
-                auto cur = make(i1), pre = make(i2);
-                next_idx2class[i2] = next_idx2class[i1] + (cur != pre);
+                size_type i1 = ord2idx[i], i2 = ord2idx[i - 1];
+                bool same = [&] {
+                    if (sz <= i1 + len) return false;
+                    if (sz <= i2 + len) return false;
+                    if (buf[i1] != buf[i2]) return false;
+                    if (buf[i1 + len / 2] != buf[i2 + len / 2]) return false;
+                    return true;
+                }();
+                if (same) {
+                    idx2class[i1] = idx2class[i2];
+                } else {
+                    idx2class[i1] = i;
+                }
             }
-            std::swap(next_idx2class, idx2class);
+
+            // sort
+            std::iota(ALL(cnts), 0ll);
+            buf = ord2idx;
+            for (size_type i = 0; i < sz; i++) {
+                auto idx = buf[i];
+                if (len <= idx) ord2idx[cnts[idx2class[idx - len]]++] = idx - len;
+            }
         };
 
         for (size_type k = 0; (1ll << k) < sz; k++) build_step(k);
@@ -86,20 +87,20 @@ class SuffixArray {
     }
 
     size_type lower_bound_aux(const Container &oth) const noexcept {
-        size_type ng = -1, ok = size() - 1;
+        ll ng = -1, ok = size() - 1;
         while (std::abs(ok - ng) > 1) {
             auto mid = (ok + ng) / 2;
-            auto res = comp(oth, sa[mid], 0);
+            auto res = comp(oth, ord2idx[mid], 0);
             (check_lower_bound(res) ? ok : ng) = mid;
         }
         return ok;
     }
 
     size_type upper_bound_aux(const Container &oth) const noexcept {
-        size_type ng = -1, ok = size() - 1;
+        ll ng = -1, ok = size() - 1;
         while (abs(ok - ng) > 1) {
             auto mid = (ok + ng) / 2;
-            auto res = comp(oth, sa[mid], 0);
+            auto res = comp(oth, ord2idx[mid], 0);
             (check_upper_bound(res) ? ok : ng) = mid;
         }
         return ok;
@@ -134,15 +135,19 @@ public:
     }
 
     size_type lower_bound(const std::string &oth) const noexcept {
-        auto res = comp(oth, sa.back(), 0);
+        auto res = comp(oth, ord2idx.back(), 0);
         if (!check_lower_bound(res)) return size();
         return lower_bound_aux(oth);
     }
 
     size_type upper_bound(const std::string &oth) const noexcept {
-        auto res = comp(oth, sa.back(), 0);
+        auto res = comp(oth, ord2idx.back(), 0);
         if (!check_upper_bound(res)) return size();
         return upper_bound_aux(oth);
+    }
+
+    const Container& raw() const noexcept {
+        return s;
     }
 };
 
